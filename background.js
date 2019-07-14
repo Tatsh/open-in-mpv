02e985cf8eb23d535ff061d1281a41aa0e7fe4ac
Andrew Udvare <audvare@gmail.com>
2019-07-14 09:05:50 -0400

    Nitpick

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.contextMenus.create({
   contexts: ['link'],
   title: 'Open in mpv',
-  onclick: function (info) {
-    chrome.runtime.sendNativeMessage('sh.tat.open_in_mpv', { url: info['linkUrl'] });
-  }
+  onclick: info => chrome.runtime.sendNativeMessage(
+    'sh.tat.open_in_mpv',
+    { url: info['linkUrl'] }
+  )
 });
