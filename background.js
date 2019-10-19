49093b261743ecf0c15047d55a444188f58ad02a
Andrew Udvare <audvare@gmail.com>
2019-10-19 14:06:02 -0400

    Handle other menu contexts

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.contextMenus.create({
-  contexts: ['link'],
+  contexts: ['link', 'page', 'video', 'audio'],
   title: 'Open in mpv',
-  onclick: info => chrome.runtime.sendNativeMessage(
-    'sh.tat.open_in_mpv',
-    { url: info['linkUrl'] }
-  )
+  onclick: info =>
+    chrome.runtime.sendNativeMessage('sh.tat.open_in_mpv', {
+      url: info['linkUrl'] || info['srcUrl'] || info['pageUrl']
+    })
 });
