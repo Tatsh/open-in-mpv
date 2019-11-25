7071de86c1b6adb0ddaf6cb1b12f5df03042d71f
Andrew Udvare <audvare@gmail.com>
2019-11-24 19:18:50 -0500

    Add license to file

--- src/options.js
+++ src/options.js
@@ -1,9 +1,29 @@
+/**
+ * This file is part of open-in-mpv.
+ *
+ * Copyright 2019 Andrew Udvare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 const qs = document.querySelector.bind(document);
 const button = qs('#save');
-const info = qs('#info');
-const logFile = qs('#log-file');
-const saved = qs('#saved');
-const socket = qs('#socket');
 const checkboxFields = {
   debugFlag: qs('#debug'),
   singleFlag: qs('#single')
@@ -12,6 +32,10 @@ const defaults = {
   debugFlag: false,
   singleFlag: true
 };
+const info = qs('#info');
+const logFile = qs('#log-file');
+const saved = qs('#saved');
+const socket = qs('#socket');
 const WAIT_TIME = 5000;
 
 button.addEventListener('mousedown', () => {
