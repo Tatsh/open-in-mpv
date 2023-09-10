b8d12cea54fb818ce1fd09e2c8e38053c4ab18c8
moversi <129067651+movrsi@users.noreply.github.com>
2023-09-10 01:21:05 -0400

    Type constants.

--- open_in_mpv/constants.py
+++ open_in_mpv/constants.py
@@ -12,40 +12,40 @@ IS_MAC: Final[bool] = bool(platform.mac_ver()[0])
 IS_WIN: Final[bool] = bool(platform.win32_ver()[0])
 IS_LINUX: Final[bool] = not IS_MAC and not IS_WIN
 
-JSON_FILENAME = 'sh.tat.open_in_mpv.json'
-HOME = os.environ.get('HOME', '')
+JSON_FILENAME: Final[str] = 'sh.tat.open_in_mpv.json'
+HOME: Final[str] = os.environ.get('HOME', '')
 
-USER_CHROME_HOSTS_REG_PATH_WIN = 'HKCU:\\Software\\Google\\Chrome\\NativeMessagingHosts'
+USER_CHROME_HOSTS_REG_PATH_WIN: Final[str] = 'HKCU:\\Software\\Google\\Chrome\\NativeMessagingHosts'
 
-MAC_HOSTS_DIRS = (f'{HOME}/Library/Application Support/Chromium/NativeMessagingHosts',
+MAC_HOSTS_DIRS: Final[tuple[str, str, str, str]] = (f'{HOME}/Library/Application Support/Chromium/NativeMessagingHosts',
                   f'{HOME}/Library/Application Support/Google/Chrome Beta/NativeMessagingHosts',
                   f'{HOME}/Library/Application Support/Google/Chrome Canary/NativeMessagingHosts',
                   f'{HOME}/Library/Application Support/Google/Chrome/NativeMessagingHosts')
 
 MAC_BIN_PATH: Final[str] = '/opt/local/bin'
 
-SYSTEM_HOSTS_DIRS = ('/etc/chromium/native-messaging-hosts',
+SYSTEM_HOSTS_DIRS: Final[tuple[str, str, str]] = ('/etc/chromium/native-messaging-hosts',
                      '/etc/opt/chrome/native-messaging-hosts',
                      '/etc/opt/edge/native-messaging-hosts')
-USER_HOSTS_DIRS = (
+USER_HOSTS_DIRS: Final[tuple[str, str, str, str, str]] = (
     f'{xdg.BaseDirectory.xdg_config_home}/BraveSoftware/Brave-Browser/NativeMessagingHosts',
     f'{xdg.BaseDirectory.xdg_config_home}/chromium/NativeMessagingHosts',
     f'{xdg.BaseDirectory.xdg_config_home}/google-chrome-beta/NativeMessagingHosts',
     f'{xdg.BaseDirectory.xdg_config_home}/google-chrome-canary/NativeMessagingHosts',
     f'{xdg.BaseDirectory.xdg_config_home}/google-chrome/NativeMessagingHosts')
 
-COMMON_HOST_DATA: dict[str, str | None] = {
+COMMON_HOST_DATA: Final[dict[str, str | None]] = {
     'description': 'Opens a URL in mpv (for use with extension).',
     'path': None,
     'type': 'stdio'
 }
-HOST_DATA: dict[str, str | None | Sequence[str]] = {
+HOST_DATA: Final[dict[str, str | None | Sequence[str]]] = {
     **COMMON_HOST_DATA,
     # cspell:disable-next-line
     'allowed_origins': ['chrome-extension://ggijpepdpiehgbiknmfpfbhcalffjlbj/'],
     'name': 'sh.tat.open_in_mpv',
 }
-HOST_DATA_FIREFOX: dict[str, str | None | Sequence[str]] = {
+HOST_DATA_FIREFOX: Final[dict[str, str | None | Sequence[str]]] = {
     **COMMON_HOST_DATA,
     'allowed_extensions': ['{43e6f3ef-84a0-55f4-b9dd-d879106a24a9}'],
     'name': 'sh.tat.open-in-mpv',
